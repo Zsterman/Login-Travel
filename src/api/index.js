95b63cb5b23792c772be4940ddf80087d04a2df6
Zachary Sterman <90649750+Zsterman@users.noreply.github.com>
2021-12-17 13:13:14 -0500

    updating travel app

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'x-rapidapi-key': 'c6f5967fd2msh3ff16a0d784c8ecp152ec2jsne520d946c82a',
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            }
+          });
+
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
